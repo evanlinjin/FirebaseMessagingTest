20b603a75282294d8ce407186d35e995b4f894b8
Evan Chih-Yu Lin <evan.linjin@gmail.com>
2016-09-30 17:25:51 +1300

    Appearance modifications.

--- app/src/main/java/com/linjin/firebasemessagingtest/ChatMessageList.java
+++ app/src/main/java/com/linjin/firebasemessagingtest/ChatMessageList.java
@@ -1,16 +1,13 @@
 package com.linjin.firebasemessagingtest;
 
 import android.app.Activity;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.ColorFilter;
-import android.graphics.drawable.Drawable;
 import android.text.SpannableString;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
@@ -23,6 +20,7 @@ import java.util.List;
  */
 
 public class ChatMessageList extends ArrayAdapter<ChatMessage> {
+    private final static String TAG = "ChatMessageList";
     private final static int changeHeadingTime = 60000*5; // In (ms)
     private Activity context;
     private List<ChatMessage> messages;
@@ -37,33 +35,39 @@ public class ChatMessageList extends ArrayAdapter<ChatMessage> {
 
         // Get the message we are displaying.
         ChatMessage message = messages.get(position);
-
+        ChatMessage prevmsg = messages.get(position == 0 ? 0 : position - 1);
         // Get the inflater and inflate the XML layout for each item.
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.activity_message_list_delegate, null);
+
+        View view;
+
+        // START [ Get correct message 'xml' file ]
+        if (message.uid.equals(MessagingActivity.name)) {
+            view = inflater.inflate(R.layout.msg_delegate_right, parent, false);
+        } else {
+            view = inflater.inflate(R.layout.msg_delegate_left, parent, false);
+        }
+        // END [ Get correct message 'xml' file ]
 
         TextView time_label = (TextView) view.findViewById(R.id.time_label);
-        time_label.setText(
-                getStringFromEpoch(message.epoch, position == 0 ? 0 : messages.get(position - 1).epoch)
-        );
-        if (time_label.getText().toString().isEmpty()) {
-            ViewGroup.LayoutParams params = time_label.getLayoutParams();
-            params.height = 0;
-            time_label.setLayoutParams(params);
-        } else {
-            time_label.setPadding(0, getPxFromDp(5), 0, 0);
-        }
-
+        TextView time_label2 = (TextView) view.findViewById(R.id.time_label2);
         TextView message_label = (TextView) view.findViewById(R.id.message_label);
         SpannableString msgText;
 
+        // START [ Set time label ]
+        SimpleDateFormat format = new SimpleDateFormat("dd.MM.yy");
+        String dateStr = format.format(new Date(message.epoch));
+        String prvdStr = format.format(new Date(prevmsg.epoch));
+        if (!dateStr.equals(prvdStr) || position == 0) {
+            time_label.setVisibility(View.VISIBLE);
+            time_label.setText(dateStr);
+        }
+        format = new SimpleDateFormat("HH:mm");
+        time_label2.setText(format.format(new Date(message.epoch)));
+        // END [ Set time label ]
+
         if (message.uid.equals(MessagingActivity.name)) {
-//            message_label.setTextAlignment(View.TEXT_ALIGNMENT_TEXT_END);
             msgText = new SpannableString(String.valueOf(message.msg));
-            view.findViewById(R.id.bubble).setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
-            view.findViewById(R.id.icon).setVisibility(View.INVISIBLE);
-            message_label.setBackgroundResource(R.drawable.colored_sqircle);
-            message_label.setTextColor(Color.WHITE);
         }
         else {
             msgText = new SpannableString(String.valueOf(message.uid + "\n" + message.msg));
@@ -71,23 +75,9 @@ public class ChatMessageList extends ArrayAdapter<ChatMessage> {
                     new android.text.style.StyleSpan(android.graphics.Typeface.BOLD),
                     0, message.uid.length(), 0
             );
-            view.findViewById(R.id.icon).setVisibility(View.GONE);
+//            view.findViewById(R.id.icon).setVisibility(View.GONE);
         }
         message_label.setText(msgText);
         return view;
-    }
-
-    private String getStringFromEpoch(long epoch, long epoch_old) {
-        if (epoch - epoch_old > changeHeadingTime) {
-            Date date = new Date(epoch);
-            SimpleDateFormat format = new SimpleDateFormat("HH:mm (dd/MM/yy)");
-            return format.format(date);
-        }
-        return "";
-    }
-
-    private int getPxFromDp(int dp) {
-        final float scale = getContext().getResources().getDisplayMetrics().density;
-        return (int) (dp * scale + 0.5f);
     }
 }
